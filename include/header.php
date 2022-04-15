dc4a8738aff7ef5e1c08bc04f382f7ba4c11cd4d
Siamak <turksiam0@gmail.com>
2022-04-15 23:17:39 +0430

    Template: Tabriz City - Beta

--- include/header.php
+++ include/header.php
@@ -1,27 +1,27 @@
     <!-- ======= Header ======= -->
-    
+
     <header class="overflow-hidden">
 
         <nav class="py-2 bg-light border-bottom">
             <div class="container d-flex flex-wrap">
                 <ul class="nav me-auto">
-                    <li class="nav-item"><a href="./index.php" class="nav-link link-dark px-2 active" aria-current="page">خانه</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link link-dark px-2">درباره تبریز</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link link-dark px-2">اطلاعات شهری</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link link-dark px-2">گردشگری</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link link-dark px-2">تماس با ما</a></li>
+                    <li class="nav-item nav-ul-li"><a href="./index.php" class="nav-link link-dark px-2 active" aria-current="page">خانه</a></li>
+                    <li class="nav-item nav-ul-li"><a href="#" class="nav-link link-dark px-2">درباره تبریز</a></li>
+                    <li class="nav-item nav-ul-li"><a href="#" class="nav-link link-dark px-2">اطلاعات شهری</a></li>
+                    <li class="nav-item nav-ul-li"><a href="#" class="nav-link link-dark px-2">گردشگری</a></li>
+                    <li class="nav-item nav-ul-li"><a href="#" class="nav-link link-dark px-2">تماس با ما</a></li>
                 </ul>
-                <ul class="nav">
-                    <li class="nav-item"><a href="./login.php" class="nav-link link-dark px-2">ورود</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link link-dark px-2">ثبت نام</a></li>
-                </ul>
+                <div class="btn-group">
+                    <a href="./login.php" class="btn btn-outline-primary">ورود</a>
+                    <a href="#" class="btn btn-outline-success">ثبت نام</a>
+                </div>
             </div>
         </nav>
 
         <div class="py-3 mb-4 border-bottom">
             <div class="container d-flex flex-wrap justify-content-center">
                 <a href="./index.html" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
-                    <span class="fs-4 fw-bolder">تبریز | شهر اولین ها</span>
+                    <span class="fs-4 fw-bolder title-web-name">تبریز | شهر اولین ها</span>
                 </a>
                 <form class="col-12 col-lg-auto mb-3 mb-lg-0">
                     <input type="search" class="form-control" placeholder="دنبال چی میگردی؟" aria-label="Search">
