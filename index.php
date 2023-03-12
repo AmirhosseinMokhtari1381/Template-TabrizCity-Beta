03399e38f7302a79797323550e042a9e81e1fa1e
Amirhossein <amirhossein.mokhtari120@gmail.com>
2023-03-13 00:31:13 +0330

    editResturant

--- index.php
+++ index.php
@@ -136,7 +136,7 @@
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="headingTwo">
                                 <button class="accordion-button fw-bolder collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    بهترین رستوران های تبریز کدام ها هستند؟
+                                    بهترین غذاخوری های تبریز کدام ها هستند؟
                                 </button>
                             </h2>
                             <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
